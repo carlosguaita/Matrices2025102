7c4593c886d582232f59c1ea07eb166a5773ec80
carlos.guaita <carlos.guaita@udla.edu.ec>
2024-10-01 14:58:26 -0500

    inicio

---
+++ main2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+    char nombres[5][20];
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Ingrese el nombre %d: ",i);
+        fflush(stdin);
+        //scanf("%s",&nombres[i]);
+        fgets(nombres[i],20,stdin);
+    }
+    
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s",nombres[i]);
+    }
+    
+
+    return 0;
+}
